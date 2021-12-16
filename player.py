ad8453088aa32ee44561ad06f39e07377ba96849
Aeijan Bajracharya <aeijan.bajracharya@intel.com>
2021-12-16 23:40:47 +0000

    Created a class for each player in the game

---
+++ player.py
@@ -0,0 +1,29 @@
+import random
+
+from deck import Deck
+from collections import deque
+
+
+class Player(Deck):
+    def __init__(self, name):
+        self.name = name
+        self.cards = deque()
+
+    def get_name(self):
+        return self.name
+
+    def add_card(self, card):
+        self.cards.append(card)
+        return self
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def prepend_cards(self, winnings):
+        random.shuffle(winnings)
+        for c in winnings:
+            self.cards.appendleft(c)
+
+    def show_cards(self):
+        for c in self.cards:
+            c.show()
