679ff804f009d3186299a435bf0fcdd322ba1e58
Aeijan Bajracharya <aeijan.bajracharya@intel.com>
2021-12-16 23:42:50 +0000

    Created a main file to run the Game class

---
+++ main.py
@@ -0,0 +1,29 @@
+# Main Python file for the War card game
+
+from deck import Deck
+from game import Game
+from player import Player
+
+def main():
+    Game()
+    # deck = Deck()
+    # deck.shuffle()
+    # # deck.show()
+    #
+    # player = Player("Crackhead")
+    # player.draw(deck).draw(deck)
+    # player.show_cards()
+    # # game = Game("Hi")
+    # # game.printthis()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
