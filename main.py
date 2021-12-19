2196d9fc5597ed296a019789ecd72c6b4ba66e2b
Aeijan <bajracae@oregonstate.edu>
2021-12-18 22:45:34 -0800

    Removed commented out code

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ from player import Player
 
 def main():
     Game()
-    # deck = Deck()
-    # deck.shuffle()
-    # # deck.show()
-    #
-    # player = Player("Crackhead")
-    # player.draw(deck).draw(deck)
-    # player.show_cards()
-    # # game = Game("Hi")
-    # # game.printthis()
 
 if __name__ == "__main__":
     main()
