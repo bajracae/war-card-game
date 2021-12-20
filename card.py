34c95a387700f71fa2d365ab275940e8ec03159d
Aeijan <bajracae@oregonstate.edu>
2021-12-20 10:47:51 -0800

    Added functions to print cards in ascii format

--- card.py
+++ card.py
@@ -9,3 +9,24 @@ class Card(object):
 
     def show(self):
         print(self.value + " of " + self.suit)
+
+    def ascii_card(self):
+        print("┌───────┐")
+        print("| {:<2}    |".format(self.value))
+        print("|       |")
+        print("|   {}   |".format(self.suit[0]))
+        print("|       |")
+        print("|    {:>2} |".format(self.value))
+        print("└───────┘") 
+
+    def ascii_card_three_face_down(self):
+        print("┌───────┐")
+        print("|┌───────┐")
+        print("||┌───────┐")
+        print("|||┌───────┐")
+        print("|||| {:<2}    |".format(self.value))
+        print("||||       |")
+        print("└|||   {}   |".format(self.suit[0])) 
+        print(" └||       |")
+        print("  └|    {:>2} |".format(self.value))
+        print("   └───────┘")
