39dcd2d53901301ab2dde37416aad496fadf49a0
Aeijan <bajracae@oregonstate.edu>
2021-12-21 12:12:28 -0800

    Refactored and added comments to clarify complex code

--- game.py
+++ game.py
@@ -15,15 +15,16 @@ class Game(object):
     def run_game(self):
         self.header()
 
+        # Name of the players
         computer = "Computadora"
         player = self.get_player_name_from_user()
 
-        # Create players of the game
+        # List of Player objects each representing a player
         players = [Player(computer), Player(player)]
 
         deck = Deck()
 
-        # Deal out the cards
+        # Deal out the cards to each player
         while len(deck.deck) > 0:
             for p in players:
                 p.add_card(deck.draw_card())
@@ -38,11 +39,12 @@ class Game(object):
 
         # Draw one card from each player's pile
         while True:
-            # Player 1, Player 2 fields
+            # Lists to store Player 1's and 2's played cards in a round
             field = [[], []]
 
             _ = self.get_game_input_from_user()
             
+            # Flag sets to True when war takes place
             in_war = False
 
             while True:
@@ -52,7 +54,7 @@ class Game(object):
                 p1_card_rank = field[0][-1].get_rank()
                 p2_card_rank = field[1][-1].get_rank()
 
-                # Draw out the cards
+                # Print different ascii arts based the status of the in_war flag
                 if in_war == True:
                     print("*******************************************************************")
                     print("{}'s card".format(colored(players[0].get_name(), "red")))
@@ -68,6 +70,7 @@ class Game(object):
                     print("{}'s card".format(colored(players[1].get_name(), "green")))
                     field[1][-1].ascii_card()
 
+                # Check players' card ranks and place won cards into the bottom of the winner's deck, activate war if ranks are equal
                 if p1_card_rank > p2_card_rank:
                     all_cards = [j for i in field for j in i]
                     players[0].prepend_cards(all_cards)
@@ -85,18 +88,13 @@ class Game(object):
                     print("WAR!")
                     in_war = True
                     
+                    # Check if both players have enough cards to engage in war, each player needed at least 4 cards
                     if players[0].num_cards() < 4 and players[1].num_cards() >= 4:
                         print("{} doesn't have enough cards to wage war. {} wins!".format(colored(players[0].get_name(), "red"), colored(players[1].get_name(), "green")))
                         exit()
-
                     elif players[1].num_cards() < 4 and players[0].num_cards() >= 4:
                         print("{} doesn't have enough cards to wage war. {} wins!".format(colored(players[1].get_name(), "green"), colored(players[0].get_name(), "red")))
                         exit()
-
-                    elif players[0].num_cards() < 4 and players[1].num_cards() < 4:
-                        print("Both players don't have enough cards to wage war. The game has ended in a tie.")
-                        exit()
-
                     else:
                         print("Placing three cards down and flipping the fourth card...")
                         time.sleep(1)
@@ -104,11 +102,11 @@ class Game(object):
                             field[0].append(players[0].pop_card())
                             field[1].append(players[1].pop_card())
 
-
+            # If one player runs out of cards, the other player wins
             if players[0].num_cards() == 0:
                 print("{} wins!".format(colored(players[1].get_name(), "green")))
                 break
-
+            
             if players[1].num_cards() == 0:
                 print("{} wins!".format(colored(players[0].get_name(), "red")))
                 break
