463838ecb6a40dc8a540322e9f34081a69258ec2
Aeijan <bajracae@oregonstate.edu>
2021-12-21 11:26:01 -0800

    Implemented all known game logic and user interface features

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import time
+
 from deck import Deck
 from player import Player
 
@@ -5,7 +7,6 @@ from colorama import init
 from pyfiglet import Figlet
 from termcolor import colored
 
-import time
 
 class Game(object):
     def __init__(self):
@@ -14,17 +15,13 @@ class Game(object):
     def run_game(self):
         self.header()
 
-        deck = Deck()
-
-        player = input("Enter your name: ")
-        
-        while player == "":
-            player = input("Error, enter your name: ")
-
         computer = "Computadora"
+        player = self.get_player_name_from_user()
 
         # Create players of the game
         players = [Player(computer), Player(player)]
+
+        deck = Deck()
 
         # Deal out the cards
         while len(deck.deck) > 0:
@@ -37,23 +34,14 @@ class Game(object):
 
         time.sleep(1)
 
-        print("*******************************************************************")
-        print("[SCOREBOARD]")
-        print("{}: {} cards | {}: {} cards".format(computer, players[0].num_cards(), player, players[1].num_cards()))
-        print("*******************************************************************")
+        self.scoreboard(players)
 
         # Draw one card from each player's pile
         while True:
             # Player 1, Player 2 fields
             field = [[], []]
 
-            user_input = input("Press enter to continue or 'q' to quit the game: ")
-            while user_input != "":
-                if user_input == "q":
-                    print("Quitting the game...")
-                    time.sleep(0.5)
-                    exit()
-                user_input = input("Error, invalid input. Try again: ")
+            _ = self.get_game_input_from_user()
             
             in_war = False
 
@@ -67,30 +55,30 @@ class Game(object):
                 # Draw out the cards
                 if in_war == True:
                     print("*******************************************************************")
-                    print("{}'s card".format(computer))
+                    print("{}'s card".format(colored(players[0].get_name(), "red")))
                     field[0][-1].ascii_card_three_face_down()
                     print("*******************")
-                    print("{}'s card".format(player))
+                    print("{}'s card".format(colored(players[1].get_name(), "green")))
                     field[1][-1].ascii_card_three_face_down()
                 else:
                     print("*******************************************************************")
-                    print("{}'s card".format(computer))
+                    print("{}'s card".format(colored(players[0].get_name(), "red")))
                     field[0][-1].ascii_card()
                     print("*******************")
-                    print("{}'s card".format(player))
+                    print("{}'s card".format(colored(players[1].get_name(), "green")))
                     field[1][-1].ascii_card()
 
                 if p1_card_rank > p2_card_rank:
                     all_cards = [j for i in field for j in i]
                     players[0].prepend_cards(all_cards)
                     in_war = False
-                    print("{} won this round!".format(computer))
+                    print("{} won this round! (╥_╥)".format(colored(players[0].get_name(), "red")))
                     break
                 elif p1_card_rank < p2_card_rank:
                     all_cards = [j for i in field for j in i]
                     players[1].prepend_cards(all_cards)
                     in_war = False
-                    print("{} won this round!".format(player))
+                    print("{} won this round! (^__^)".format(colored(players[1].get_name(), "green")))
                     break
                 else:
                     print("*******************************************************************")
@@ -98,11 +86,11 @@ class Game(object):
                     in_war = True
                     
                     if players[0].num_cards() < 4 and players[1].num_cards() >= 4:
-                        print("{} doesn't have enough cards to wage war. {} wins!".format(computer, player))
+                        print("{} doesn't have enough cards to wage war. {} wins!".format(colored(players[0].get_name(), "red"), colored(players[1].get_name(), "green")))
                         exit()
 
                     elif players[1].num_cards() < 4 and players[0].num_cards() >= 4:
-                        print("{} doesn't have enough cards to wage war. {} wins!".format(player, computer))
+                        print("{} doesn't have enough cards to wage war. {} wins!".format(colored(players[1].get_name(), "green"), colored(players[0].get_name(), "red")))
                         exit()
 
                     elif players[0].num_cards() < 4 and players[1].num_cards() < 4:
@@ -118,18 +106,15 @@ class Game(object):
 
 
             if players[0].num_cards() == 0:
-                print("{} wins!".format(player))
+                print("{} wins!".format(colored(players[1].get_name(), "green")))
                 break
 
             if players[1].num_cards() == 0:
-                print("{} wins!".format(computer))
+                print("{} wins!".format(colored(players[0].get_name(), "red")))
                 break
 
             print("\n")
-            print("*******************************************************************")
-            print("[SCOREBOARD]")
-            print("{}: {} cards | {}: {} cards".format(computer, players[0].num_cards(), player, players[1].num_cards()))
-            print("*******************************************************************")
+            self.scoreboard(players)
 
     def header(self):
         init()
@@ -138,6 +123,32 @@ class Game(object):
         print(colored(f.renderText("Let's Play War!"), "red"), end="")
         print("*******************************************************************")
         print("Welcome to an implementation of War, a two-player card game that")
-        print("you can be played to help past the time. Please read the README.md")
-        print("to learn how to play.")
+        print("you can play pass the time. Please read the README.md to learn")
+        print("how to play the game.")
         print("*******************************************************************")
+
+    def scoreboard(self, players):
+        print("*******************************************************************")
+        print(colored("[ SCOREBOARD ]", "yellow"))
+        print(colored("{}: {} cards".format(players[0].get_name(), players[0].num_cards()), "red") + " | " + colored("{}: {} cards".format(players[1].get_name(), players[1].num_cards()), "green")) 
+        print("*******************************************************************")
+
+    def get_player_name_from_user(self):
+        name = input("Please enter your first name: ")
+
+        while name == "":
+            name = input("Invalid input found. Please enter your first name: ")
+
+        return name
+    
+    def get_game_input_from_user(self):
+        user_input = input("Press 'ENTER' to play your next card / 'q' to quit the game: ")
+
+        while user_input != "":
+            if user_input == "q":
+                print("Quitting the game...")
+                time.sleep(0.5)
+                exit()
+            user_input = input("Invalid input found. Please try again: ")
+        
+        return user_input
